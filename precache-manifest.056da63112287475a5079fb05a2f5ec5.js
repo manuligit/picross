377cdaf3eaed98f7306d64e7cba48e1d7b0ea195
manuligit <riina.vaisanen@gmail.com>
2018-10-22 14:28:29 +0300

    Updates

---
+++ precache-manifest.056da63112287475a5079fb05a2f5ec5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "764344afe87ab1510d31",
+    "url": "/picross/static/js/runtime~main.764344af.js"
+  },
+  {
+    "revision": "ea48ef08d123fc88bae6",
+    "url": "/picross/static/js/main.ea48ef08.chunk.js"
+  },
+  {
+    "revision": "2b037c810d58ac10280f",
+    "url": "/picross/static/js/1.2b037c81.chunk.js"
+  },
+  {
+    "revision": "ea48ef08d123fc88bae6",
+    "url": "/picross/static/css/main.51cd3db3.chunk.css"
+  },
+  {
+    "revision": "0259bbce6b319ff40951a903815d78dc",
+    "url": "/picross/index.html"
+  }
+];
